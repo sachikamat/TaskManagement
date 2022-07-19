267b8ed306d783fea4907371206a0312147cea0a
sachikamat <sachikamat1@gmail.com>
2022-07-19 09:51:02 +0545

    'UpdatedLogin'

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,20 +1,22 @@
 import React from 'react'
-import './Login.css'
 import {Paper, Button, TextField, Grid, FormControlLabel} from '@material-ui/core';
 import { CheckBox } from '@material-ui/icons';
 
 export default function Login() {
 
-  const paperStyle={padding:20, width:280, margin:'20px auto',bgcolor:'#00d563'}
-  const login_btn={backgroundColor: "#00d563", color:'white',fontWeight:400,marginTop:20,marginBottom:30}
+  const paperStyle={padding:30, width:280, margin:'20px auto',bgcolor:'#00d563'}
+  const login_btn={backgroundColor: "#00d563", color:'white',fontWeight:400,marginTop:30,marginBottom:30}
+  const logoStyle={height:60,width:60,margin:20}
   return (
     
     <Grid>
       <Paper elevation={10} style={paperStyle}>
           <Grid>
-            <h1>LOG IN</h1>
+            
+            <img src="asterLogo.png" alt="" style={logoStyle} />
+            <h1 style={{fontSize:20}}>LOG IN</h1>
           </Grid>
-          <TextField name='username' id='username' label='Username' type='email' placeholder='Enter username' fullWidth required style={{my:20}} ></TextField>
+          <TextField name='username' id='username' label='Username' type='email' placeholder='Enter username' fullWidth required style={{marginBottom:20}} ></TextField>
           <TextField name='password' id='password' label='Password' type='password' placeholder='Enter password' fullWidth required style={{marginBottom:20}}></TextField>
           <FormControlLabel
             control={
